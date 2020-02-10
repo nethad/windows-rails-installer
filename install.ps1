c8cb5c1e14c28214ab0236428678d5dc7aa2be1f
Thomas Ritter <thomas.ritter@mocoapp.com>
2020-02-10 23:47:13 +0100

    Extract installer links, remove commented code

--- install.ps1
+++ install.ps1
@@ -2,6 +2,15 @@ $RubyInstallerVersion = "2.6.5-1"
 $NodeJsVersion = "12.13.0"
 $GitVersion = "2.25.0"
 
+# *****************************************************************
+# If you want to fetch files from a different (e.g. local) server
+# change these installer links to a local source.
+# *****************************************************************
+$RubyInstallerLink = "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-$RubyInstallerVersion/rubyinstaller-devkit-$RubyInstallerVersion-x64.exe"
+$NodeJsLink = "https://nodejs.org/dist/v$NodeJsVersion/node-v$NodeJsVersion-x64.msi"
+$YarnLink = "https://yarnpkg.com/latest.msi"
+$GitLink = "https://github.com/git-for-windows/git/releases/download/v$GitVersion.windows.1/Git-$GitVersion-64-bit.exe"
+
 function refresh-path {
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") +
                 ";" +
@@ -14,14 +23,17 @@ cd railsgirls
 
 $CurrentDir = Convert-Path .
 
+"."
+"."
+"."
 "Downloading RubyInstaller... this could take a while..."
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-$RubyInstallerVersion/rubyinstaller-devkit-$RubyInstallerVersion-x64.exe", "$CurrentDir\rubyinstaller.exe")
+(New-Object System.Net.WebClient).DownloadFile($RubyInstallerLink, "$CurrentDir\rubyinstaller.exe")
 "Downloading Yarn... this could take a while..."
-(New-Object System.Net.WebClient).DownloadFile("https://nodejs.org/dist/v$NodeJsVersion/node-v$NodeJsVersion-x64.msi", "$CurrentDir\nodejs.msi")
+(New-Object System.Net.WebClient).DownloadFile($NodeJsLink, "$CurrentDir\nodejs.msi")
 "Downloading NodeJS... this could take a while..."
-(New-Object System.Net.WebClient).DownloadFile("https://yarnpkg.com/latest.msi", "$CurrentDir\yarn.msi")
+(New-Object System.Net.WebClient).DownloadFile($YarnLink, "$CurrentDir\yarn.msi")
 "Downloading Git... this could take a while..."
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/git-for-windows/git/releases/download/v$GitVersion.windows.1/Git-$GitVersion-64-bit.exe", "$CurrentDir\git.exe")
+(New-Object System.Net.WebClient).DownloadFile($GitLink, "$CurrentDir\git.exe")
 
 if (!(Test-Path rubyinstaller.exe)) {
   "###########################################################################"
@@ -71,8 +83,6 @@ if (!(Test-Path git.exe)) {
 
 "Installing RubyInstaller..."
 $p = Start-Process $CurrentDir\rubyinstaller.exe -ArgumentList "/silent" -wait -NoNewWindow -PassThru
-# $p.HasExited
-# $p.ExitCode
 
 "Installing Git..."
 $p = Start-Process $CurrentDir\git.exe -ArgumentList "/silent" -wait -NoNewWindow -PassThru
@@ -109,6 +119,7 @@ bundle install
 "# ALL DONE.                             #"
 "#                                       #"
 "# Now enter:                            #"
+"#                                       #"
 "# bundle exec rails server              #"
 "#                                       #"
 "# and visit http://localhost:3000       #"
