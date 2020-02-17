ddc77444ff774a589d76027c47a3121fce8737f2
Thomas Ritter <thomas.ritter@mocoapp.com>
2020-02-17 22:59:48 +0100

    Improve server script

--- server.ps1
+++ server.ps1
@@ -1,4 +1,24 @@
 ipconfig
-ruby -rwebrick -e'WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => Dir.pwd).start'
+
+"."
+"."
+"."
+
+$HostIp = (
+    Get-NetIPConfiguration |
+    Where-Object {
+        $_.IPv4DefaultGateway -ne $null -and
+        $_.NetAdapter.Status -ne "Disconnected"
+    }
+).IPv4Address.IPAddress
+
+"###############################################################################"
+""
 "Now visit whatever IP you see above that is reachable in the local network"
-"e.g.: http://10.0.23.42:8000/howto.html.rhtml"
+""
+"most probably: http://" + $HostIp + ":8000/howto.html.rhtml"
+""
+"###############################################################################"
+""
+
+ruby -rwebrick -e'WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => Dir.pwd).start'
